36f711f5873808f15065d0ecf96eebc2fd779a88
TheBookwyrms <blackdragon6493@gmail.com>
2024-12-14 23:20:43 -0500

    some reformatting

--- basic_math_and_functions.py
+++ basic_math_and_functions.py
@@ -1,12 +1,10 @@
-def pi9():
-    pi = 3.14159265
-    return pi
-
-
-def pi100():
-    pi = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679
-    return pi
-
+class pi_digits:
+    def __init__(self, num):
+        if (num > 100) or (num < 0) or (type(num) != int):
+            raise ValueError("pi: 0 to 100 digits, int")
+        
+        pi = "3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679"
+        self.pi = float(pi[:num+2])
 
 def absolute_value(x):
     if x >= 0:
@@ -15,7 +13,6 @@ def absolute_value(x):
         x = x/(-1)
     
     return x
-
 
 def factorial(x):
     x = x
--- trig_stuff.py
+++ trig_stuff.py
@@ -1,26 +1,31 @@
 import basic_math_and_functions as b
 
+pi = b.pi_digits(100).pi
+
 def taylorSeriesSine(x):
     term_1 = x
     term_2 = term_1 - ((x**3)/b.factorial(3))
     term_3 = term_2 + ((x**5)/b.factorial(5))
     term_4 = term_3 - ((x**7)/b.factorial(7))
     term_5 = term_4 + ((x**9)/b.factorial(9))
+    term_6 = term_5 - ((x**11)/b.factorial(11))
+    term_7 = term_6 + ((x**13)/b.factorial(13))
 
-    return term_5
+    return term_7
 
 def sinRad(theta):
     theta = angleFixer(theta)
-    if theta <= b.absolute_value(b.pi9()):
+    if theta <= b.absolute_value(pi):
         return taylorSeriesSine(theta)
-
+    
 def angleFixer(theta):
-    while theta > b.absolute_value(b.pi9()):
-        theta -= (2*b.pi9())
-    return theta
+    c = theta
+    while c > b.absolute_value(pi):
+        c -= (2*pi)
+    return c
 
 def cosRad(theta):
-    cos = sinRad(theta + (b.pi9()/2))
+    cos = sinRad(theta + (pi/2))
     return cos
 
 def tanRad(theta):
